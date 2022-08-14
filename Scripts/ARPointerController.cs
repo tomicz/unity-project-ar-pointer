5543c9cf9f656e65e1f5e0b79d2920ce22159bf7
tomicz <darko_darkblack@yahoo.com>
2022-08-14 19:12:11 +0200

    Updated ARPointerController: Can cast a raycast and can be enabled and disabled.

--- Scripts/ARPointerController.cs
+++ Scripts/ARPointerController.cs
@@ -1,7 +1,74 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
-public class ARPointerController : MonoBehaviour
+namespace TOMICZ.AR
 {
-    [SerializeField] private ARRaycastManager _raycastManager;
+    [RequireComponent(typeof(ARSessionOrigin))]
+    [RequireComponent(typeof(ARRaycastManager))]
+    public class ARPointerController : MonoBehaviour
+    {
+        [SerializeField] private TrackableType _trackableTypes;
+        [SerializeField] private Transform _indicatorObject = null;
+        [SerializeField] private float _trackingLostOffset = 0.5f;
+
+        [Space(10)]
+        public UnityEvent OnTrackableFoundEvent;
+        public UnityEvent OnTrackableLostEvent;
+
+        private ARRaycastManager _raycastManager;
+        private ARSessionOrigin _sessionOrigin;
+        private ARRaycaster _raycaster;
+        private bool IsIndicatorAvailable => _indicatorObject != null;
+
+        public void EnablePointer()
+        {
+            enabled = true;
+            EnableIndicator(enabled);
+        }
+
+        public void DisablePointer()
+        {
+            enabled = false;
+            EnableIndicator(enabled);
+        }
+
+        private void Awake()
+        {
+            _raycastManager = GetComponent<ARRaycastManager>();
+            _sessionOrigin = GetComponent<ARSessionOrigin>();
+            _raycaster = new ARRaycaster(_raycastManager, _sessionOrigin);
+        }
+
+        private void FixedUpdate()
+        {
+            if (!IsIndicatorAvailable)
+            {
+                return;
+            }
+
+            RegisterTrackableEvents();
+            UpdateIndicatorPosition();
+        }
+
+        private void RegisterTrackableEvents()
+        {
+            if (_raycaster.IsTrackableBeingHit)
+            {
+                OnTrackableFoundEvent?.Invoke();
+            }
+            else
+            {
+                OnTrackableLostEvent?.Invoke();
+            }
+        }
+
+        private void UpdateIndicatorPosition()
+        {
+            _indicatorObject.transform.position = _raycaster.RaycastHitPosition(_trackableTypes, _trackingLostOffset);
+        }
+
+        private void EnableIndicator(bool enabled) => _indicatorObject.gameObject.SetActive(enabled);
+    }
 }
